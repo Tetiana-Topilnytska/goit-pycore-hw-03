d7394b1ebea0e9fe739e2c4bbacaa8fdc7f41bc2
Tetiana <taniatop03@gmail.com>
2026-02-07 14:07:38 +0100

    task 2

--- homework.py
+++ homework.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+import random
 
 
+# Task 1
 def get_days_from_today(date):
     try:
         date = datetime.strptime(date, "%Y-%m-%d")
@@ -9,3 +11,15 @@ def get_days_from_today(date):
         return diff.days
     except ValueError:
         print("Invalid date")
+
+
+# Task 2
+def get_numbers_ticket(min, max, quantity):
+    if min < 1:
+        return []
+    elif max > 1000:
+        return []
+
+    numbers = random.sample(range(min, max + 1), quantity)
+    numbers.sort()
+    return numbers
